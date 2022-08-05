9d643a33a1829c7b4685d2503522f361328eed1b
Beetroot16 <vishrut.deshmukh@somaiya.edu>
2022-08-05 10:18:06 +0530

    Beet code

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
-import cv2, serial,time
+import cv2
+import serial,time
 face_cascade= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap=cv2.VideoCapture(0)
-ArduinoSerial=serial.Serial('com7',9600,timeout=0.1)
-time.sleep(1)
+fourcc= cv2.VideoWriter_fourcc(*'XVID')
+ArduinoSerial=serial.Serial('com12',9600,timeout=0.1)
+out= cv2.VideoWriter('face detection4.avi',fourcc,20.0,(640,480))
 
-while cap.isOpened():
+while True:
+    i = True
     ret, frame= cap.read()
-    frame=cv2.flip(frame,1)  #mirror the image
-    frame=cv2.flip(frame,0)  #verticle flip
+    # frame=cv2.flip(frame,1)
+    frame=cv2.flip(frame,0)  #mirror the image 
+    # print(frame.shape)
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces= face_cascade.detectMultiScale(gray,1.1,6)  #detect face
+    faces= face_cascade.detectMultiScale(gray,1.1,8)  #detect the face
     for x,y,w,h in faces:
-        #sending coordinates to Arduino
-        coordinates='X{0:d}Y{1:d}'.format((x+w//2),(y+h//2))
-        print(coordinates)
-        ArduinoSerial.write(coordinates.encode('utf-8'))
-        #center of the face
-        cv2.circle(frame,(x+w//2,y+h//2),2,(0,255,0),2)
-        #rectangle around face
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
-    #tiny rectangle
-    cv2.rectangle(frame,(640//2-30,480//2-30),
-                 (640//2+30,480//2+30),
-                  (255,255,255),3)
+        # sending coordinates to Arduino
+        string='X{0:d}Y{1:d}'.format((x+w//2),(y+h//2))
+        if i == True:
+            print(string)
+            ArduinoSerial.write(string.encode('utf-8'))
+            i = False
+        else:
+            pass
+        #plot the center of the face
+        # cv2.circle(frame,(x+w//2,y+h//2),2,(0,255,0),2)
+        #plot the roi
+        # cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+    #plot the squared region in the center of the screen
+    # cv2.rectangle(frame,(640//2-30,480//2-30),
+    #              (640//2+30,480//2+30),
+    #               (255,255,255),3)
+    out.write(frame)
+    # frame=cv2.flip(frame,0)
     cv2.imshow('img',frame)
-    
-    # press q to quit
+    cv2.imwrite('output_img.jpg',frame)
+    '''for testing purpose
+    read= str(ArduinoSerial.readline(ArduinoSerial.inWaiting()))
+    time.sleep(0.05)
+    print('data from arduino:'+read)
+    '''
+    # press q to Quit
     if cv2.waitKey(10)&0xFF== ord('q'):
         break
 cap.release()
