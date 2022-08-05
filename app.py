30dfff884c569f6d132c751d83562ba8985d7a68
Pargat-Dhanjal <pargat14@hotmail.com>
2022-08-05 17:16:12 +0530

    Updated code

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
-import cv2, serial,time
+import cv2
+import serial,time
 face_cascade= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap=cv2.VideoCapture(0)
-ArduinoSerial=serial.Serial('com7',9600,timeout=0.1)
-time.sleep(1)
+fourcc= cv2.VideoWriter_fourcc(*'XVID')
+ArduinoSerial=serial.Serial('/dev/cu.usbmodem11101',9600,timeout=0.1)
+# out= cv2.VideoWriter('face detection4.avi',fourcc,20.0,(640,480))
 
-while cap.isOpened():
+while True:
+    i = True
     ret, frame= cap.read()
-    frame=cv2.flip(frame,1)  #mirror the image
-    frame=cv2.flip(frame,0)  #verticle flip
+    frame=cv2.flip(frame,0)  # verticle flip 
+    # print(frame.shape)
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces= face_cascade.detectMultiScale(gray,1.1,6)  #detect face
+    faces= face_cascade.detectMultiScale(gray,1.1,8)  #detect the face
     for x,y,w,h in faces:
-        #sending coordinates to Arduino
-        coordinates='X{0:d}Y{1:d}'.format((x+w//2),(y+h//2))
-        print(coordinates)
-        ArduinoSerial.write(coordinates.encode('utf-8'))
-        #center of the face
-        cv2.circle(frame,(x+w//2,y+h//2),2,(0,255,0),2)
-        #rectangle around face
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
-    #tiny rectangle
-    cv2.rectangle(frame,(640//2-30,480//2-30),
-                 (640//2+30,480//2+30),
-                  (255,255,255),3)
+        # sending coordinates to Arduino
+        string='X{0:d}Y{1:d}'.format((x+w//2),((y+h//2)))
+        if i == True:
+            print(string)
+            ArduinoSerial.write(string.encode('utf-8'))
+            i = False
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(100,255,255),1)
+    # out.write(frame)
     cv2.imshow('img',frame)
-    
-    # press q to quit
+    # press q to Quit
     if cv2.waitKey(10)&0xFF== ord('q'):
         break
 cap.release()
