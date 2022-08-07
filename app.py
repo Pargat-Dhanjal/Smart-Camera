a2aabd5f134a149a5fa5bf8210c9471ed8b2c5a9
Pargat-Dhanjal <pargat14@hotmail.com>
2022-08-07 12:35:57 +0530

    Final Code

--- app.py
+++ app.py
@@ -4,27 +4,25 @@ face_cascade= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap=cv2.VideoCapture(0)
 fourcc= cv2.VideoWriter_fourcc(*'XVID')
 ArduinoSerial=serial.Serial('/dev/cu.usbmodem11101',9600,timeout=0.1)
-# out= cv2.VideoWriter('face detection4.avi',fourcc,20.0,(640,480))
 
 while True:
     i = True
     ret, frame= cap.read()
-    frame=cv2.flip(frame,0)  # verticle flip 
-    # print(frame.shape)
+    frame=cv2.flip(frame,1)  # verticle flip 
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces= face_cascade.detectMultiScale(gray,1.1,8)  #detect the face
     for x,y,w,h in faces:
         # sending coordinates to Arduino
-        string='X{0:d}Y{1:d}'.format((x+w//2),((y+h//2)))
+        coordinates='X{0:d}Y{1:d}'.format((x+w//2),((y+h//2)))
         if i == True:
-            print(string)
-            ArduinoSerial.write(string.encode('utf-8'))
+            print(coordinates)
+            ArduinoSerial.write(coordinates.encode('utf-8'))
             i = False
         cv2.rectangle(frame,(x,y),(x+w,y+h),(100,255,255),1)
-    # out.write(frame)
     cv2.imshow('img',frame)
     # press q to Quit
     if cv2.waitKey(10)&0xFF== ord('q'):
+        ArduinoSerial.write("Z".encode('utf-8'))
         break
 cap.release()
 cv2.destroyAllWindows()
--- arduino-script/arduino-script.ino
+++ arduino-script/arduino-script.ino
@@ -1,7 +1,7 @@
 #include<Servo.h>
 
 Servo x, y;
-int width = 640, height = 480;  // resolution of feed
+int width = 640, height = 480;  // resolution of camera
 int xpos = 90, ypos = 90;  // initial positions of Servos
 
 void setup() {
@@ -13,7 +13,7 @@ void setup() {
   y.write(ypos);
 }
 
-const int angle = 2;   // degree of change in angle
+const int angle = 1;   // degree of change
 
 void loop() {
   if (Serial.available() > 0)
@@ -21,28 +21,28 @@ void loop() {
     int x_mid, y_mid;
     if (Serial.read() == 'X')
     {
-      x_mid = Serial.parseInt();  // read center x-coordinate
+      x_mid = Serial.parseInt();  // read center x
       if (Serial.read() == 'Y')
-        y_mid = Serial.parseInt(); // read center y-coordinate
+        y_mid = Serial.parseInt(); // read center y
     }
+    else if(Serial.read() == 'Z'){
+      x.write(90);
+      y.write(90);
+      }
 
-// Bring servo to the center of tiny square
+// Bring servo to the center tiny square
 
-    if (x_mid > width / 2 + 30)
+    if (x_mid > width / 2 + 40)
       xpos += angle;
-    if (x_mid < width / 2 - 30)
+    if (x_mid < width / 2 - 40)
       xpos -= angle;
-    if (y_mid < height / 2 + 30)
+    if (y_mid < height / 2 + 40)
       ypos -= angle;
-    if (y_mid > height / 2 - 30)
+    if (y_mid > height / 2 - 40)
       ypos += angle;
 
-// if the servo degree is outside its range
+// if outside its range
 
-    if (xpos >= 180)
-      xpos = 180;
-    else if (xpos <= 0)
-      xpos = 0;
     if (ypos >= 180)
       ypos = 180;
     else if (ypos <= 0)
