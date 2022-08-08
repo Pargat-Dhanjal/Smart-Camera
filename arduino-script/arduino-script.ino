66eba00649fb7b7dafc7b22d7446ed651169bc4e
Pargat Singh Dhanjal <54456304+Pargat-Dhanjal@users.noreply.github.com>
2022-08-08 10:33:39 +0530

    Last commit

--- arduino-script/arduino-script.ino
+++ arduino-script/arduino-script.ino
@@ -1,56 +1,53 @@
-#include<Servo.h>
+#include <Servo.h>
 
 Servo x, y;
-int width = 640, height = 480;  // resolution of camera
-int xpos = 90, ypos = 90;  // initial positions of Servos
+int width = 640, height = 480; // Camera resoulution
+int xPos = 90, yPos = 90;      // initial positions of Servos
 
-void setup() {
-  
+void setup()
+{
   Serial.begin(9600);
   x.attach(9);
   y.attach(10);
-  x.write(xpos);
-  y.write(ypos);
+  x.write(xPos);
+  y.write(yPos);
 }
 
-const int angle = 1;   // degree of change
+const int angle = 1; // Change in degree
 
-void loop() {
+void loop()
+{
   if (Serial.available() > 0)
   {
     int x_mid, y_mid;
     if (Serial.read() == 'X')
     {
-      x_mid = Serial.parseInt();  // read center x
+      x_mid = Serial.parseInt(); // Get x coordinate from arduino
       if (Serial.read() == 'Y')
-        y_mid = Serial.parseInt(); // read center y
+        y_mid = Serial.parseInt(); // Get y coordinate from arduino
     }
-    else if(Serial.read() == 'Z'){
-      x.write(90);
-      y.write(90);
-      }
 
-// Bring servo to the center tiny square
+    // Bring servo to the center tiny square
 
-    if (x_mid > width / 2 + 40)
-      xpos += angle;
-    if (x_mid < width / 2 - 40)
-      xpos -= angle;
-    if (y_mid < height / 2 + 40)
-      ypos -= angle;
-    if (y_mid > height / 2 - 40)
-      ypos += angle;
+    if (x_mid > width / 2 + 35)
+      xPos += angle;
+    if (x_mid < width / 2 - 35)
+      xPos -= angle;
+    if (y_mid < height / 2 + 35)
+      yPos -= angle;
+    if (y_mid > height / 2 - 35)
+      yPos += angle;
 
-// if outside its range
+    // if outside y servo is out of its range
 
-    if (ypos >= 180)
-      ypos = 180;
-    else if (ypos <= 0)
-      ypos = 0;
+    if (yPos >= 180)
+      yPos = 180;
+    else if (yPos <= 0)
+      yPos = 0;
 
-    x.write(xpos);
-    y.write(ypos);
+    // Wrting the calculated values
 
- 
+    x.write(xPos);
+    y.write(yPos);
   }
 }
